032441668c370887ab78287eaddf657ce0ce151c
IvanMSP <ivanlopz11@gmail.com>
2016-12-10 13:10:55 -0600

    heroku

--- PELUDOS/wsgi.py
+++ PELUDOS/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+"""import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PELUDOS.settings")
 
 application = get_wsgi_application()
+"""
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PELUDOS.settings")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PELUDOS.wsgi --log-file -
